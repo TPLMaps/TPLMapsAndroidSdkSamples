45bfe836d60d746630c83994db1c27b6ae92edfe
hassanjamil <hassan.magma@gmail.com>
2018-12-13 17:02:49 +0500

    - Routing Sample Changes

--- samples/src/main/java/com/tplmaps/android/sdk/samples/activities/ActivityRouting.java
+++ samples/src/main/java/com/tplmaps/android/sdk/samples/activities/ActivityRouting.java
@@ -2,12 +2,14 @@ package com.tplmaps.android.sdk.samples.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.tpl.maps.sdk.routing.IMapRoute;
 import com.tpl.maps.sdk.routing.TPLRouteConfig;
 import com.tpl.maps.sdk.routing.TPLRouteManager;
 import com.tpl.maps.sdk.routing.structures.Place;
 import com.tpl.maps.sdk.routing.structures.TPLRoute;
+import com.tpl.maps.sdk.routing.structures.TPLRouteDirection;
 import com.tplmaps.android.R;
 import com.tplmaps.android.sdk.samples.utils.CommonUtilities;
 
@@ -42,7 +44,22 @@ public class ActivityRouting extends AppCompatActivity {
         new TPLRouteManager().calculate(this, config, new IMapRoute() {
             @Override
             public void onMapRoutingOverview(ArrayList<Place> endPoints, ArrayList<TPLRoute> routes) {
-                CommonUtilities.toastShort(ActivityRouting.this, "Got response");
+                String response;
+                response = "Start: " + endPoints.get(0).getY() + "," + endPoints.get(0).getX() + "\n";
+                response += "End: " + endPoints.get(1).getY() + "," + endPoints.get(1).getX() + "\n";
+
+                int routeNo = 0;
+                for (TPLRoute route : routes) {
+                    response += "\n\nRoute " + (++routeNo) + "\nRoute Length (In Meters): " + route.getTotalLength() + "\n"
+                            + "Route Time (In Milliseconds): " + route.getTotalTime() + "\n";
+                    response += "Turns: \n";
+                    for (TPLRouteDirection routeDirection : route.getListRouteDirections()) {
+                        response += routeDirection.getCompleteText() + "\n";
+                    }
+                    response += "-----------------------------------";
+                }
+
+                ((TextView) findViewById(R.id.tvResponse)).setText(response);
             }
         });
     }
